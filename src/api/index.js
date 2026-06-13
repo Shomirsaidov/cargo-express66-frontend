8b4dd61b417ce19b123575c249e0064db001f050
Shomirsaidov <abubakr.shomirsadov2@gmail.com>
2026-06-13 13:12:12 +0500

    bismillah

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || '/api',
+  baseURL: 'https://cargo-express66-backend.onrender.com/api',
   timeout: 15000,
   headers: {
     'Content-Type': 'application/json'
